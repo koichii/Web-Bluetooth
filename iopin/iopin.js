fd257df53ebac1a4785d7bb063156c9e3cd6c331
Koichi <koichii@live.jp>
2018-08-11 20:18:17 +0900

    Update iopin.js

--- iopin/iopin.js
+++ iopin/iopin.js
@@ -1,3 +1,32 @@
+
+
+
+var opts = {
+  angle: 0.15, // The span of the gauge arc
+  lineWidth: 0.44, // The line thickness
+  radiusScale: 1, // Relative radius
+  pointer: {
+    length: 0.6, // // Relative to gauge radius
+    strokeWidth: 0.035, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: false,     // If true, the min value of the gauge will be fixed
+  colorStart: '#6FADCF',   // Colors
+  colorStop: '#8FC0DA',    // just experiment with them
+  strokeColor: '#E0E0E0',  // to see which ones work best for you
+  generateGradient: true,
+  highDpiSupport: true,     // High resolution support
+  
+};
+var target = document.getElementById('foo'); // your canvas element
+var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+gauge.maxValue = 3000; // set max gauge value
+gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gauge.animationSpeed = 32; // set animation speed (32 is default value)
+gauge.set(1150); // set actual value
+
+
 /* define Parameters **************************************************************/
 // BLEデバイス名接頭句 90
 const DEVICE_NAME_PREFIX = 'BBC micro:bit'
@@ -85,6 +114,7 @@ function handleCharacteristicValueChanged(event) {
 	let value = event.target.value.getUint8(1);
 	console.log(value);
 	document.js.x.value = value
+	gauge.set(value);
 }
 	 
 // start service event
